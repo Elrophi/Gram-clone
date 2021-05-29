d275e729cdc559843558bf03f9de2e36896a5a0d
el <elrophilskwaila@gmail.com>
2021-05-29 23:20:59 +0300

    added post model

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
-from .views import Index
+from .views import Index, Post
+from .views import PostListView
 
 
 urlpatterns=[
     path('', Index.as_view(), name='index' ),
+    path('post-list/', PostListView.as_view(), name='post-list' ),
     path('accounts/', include('allauth.urls')),
     
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-created_on')
+
+        context = {
+            'post_list': posts,
+        }
+
+        return render(request, 'post_list.html', context)
+
+
+    def post(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+
+        context = {
+            'post_list': posts,
+            'form': form,
+        }
+        return render(request, 'post_list.html', context)
